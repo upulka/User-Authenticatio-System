32025db43a07117ba771a1ae0fba87a87c391a93
upulka <upulka.saku@gmail.com>
2022-10-08 01:36:02 +0530

    Adding tokens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {BrowserRouter , Route} from 'react-router-dom';
+import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
 
 function App() {
     return (
         <BrowserRouter>
+            <Route exact path = "/home">
+                <Home/>
+            </Route>
             <Route path = "/login">
                 <Login/>
             </Route>
